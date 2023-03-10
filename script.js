e09498b6c56cf65e9259d68ce77f00f152dc9afa
dileeka1998 <dileeka7777@gmail.com>
2023-03-10 11:02:30 +0530

    created game logic & added h1

--- script.js
+++ script.js
@@ -26,5 +26,32 @@ document.addEventListener('DOMContentLoaded',()=>{
         go = go === 'circle' ? 'cross' : 'circle'
         information.textContent='It is now' + go + "'s go."
         e.target.removeEventListener('click',addGo)
+        checkScore()
+    }
+
+    function checkScore(){
+        const allSquares = document.querySelectorAll('.square')
+        
+        const winnigCombos=[
+            [0,1,2],[3,4,5],[6,7,8],
+            [0,3,6],[1,4,7],[2,5,8],
+            [0,4,8],[2,4,6]
+        ]
+        winnigCombos.forEach(array=>{
+            const circleWins=array.every(cell=>allSquares[cell].firstChild?.classList.contains('circle'))
+            if(circleWins) {
+                information.textContent='Circle Wins!'
+                allSquares.forEach(element=>element.replaceWith(element.cloneNode(true)))
+                return
+            }
+        })
+        winnigCombos.forEach(array=>{
+            const crossWins=array.every(cell=>allSquares[cell].firstChild?.classList.contains('cross'))
+            if(crossWins) {
+                information.textContent='Cross Wins!'
+                allSquares.forEach(element=>element.replaceWith(element.cloneNode(true)))
+                return
+            }
+        })
     }
 })
