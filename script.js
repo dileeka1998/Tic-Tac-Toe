92d1bffb90bc34ef9ef27c15dd238e9e77d048f2
dileeka1998 <dileeka7777@gmail.com>
2023-03-10 10:42:07 +0530

    created circle & cross and made them functional

--- script.js
+++ script.js
@@ -2,16 +2,29 @@ document.addEventListener('DOMContentLoaded',()=>{
     const gameBoard = document.querySelector('#game-board')
     const information = document.querySelector('#info')
     
+    let go = 'circle'
+    information.textContent='circle goes first'
+
     const boardCells = [
         "","","","","","","","",""
         ]
         function createBoard() {
-            boardCells.forEach((cell,index)=>{
+            boardCells.forEach((_cell,index)=>{
                 const element = document.createElement('div')
                 element.classList.add('square')
+                element.id = index
+                element.addEventListener('click',addGo)
                 gameBoard.append(element)
             })
         }
     
     createBoard()
+    function addGo(e) {
+        const divCircle=document.createElement('div')
+        divCircle.classList.add(go)
+        e.target.append(divCircle)
+        go = go === 'circle' ? 'cross' : 'circle'
+        information.textContent='It is now' + go + "'s go."
+        e.target.removeEventListener('click',addGo)
+    }
 })
